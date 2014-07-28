76df1f4b8f1e638b1d0bfd45506747fbff34c2a2
ruthwickp <ruthwickp@yahoo.com>
2014-07-27 18:42:56 -0700

    Starting files for project

---
+++ MapConnect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapConnect
+//
+//  Created by Ruthwick Pathireddy on 7/27/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapConnect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapConnect
+//
+//  Created by Ruthwick Pathireddy on 7/27/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
